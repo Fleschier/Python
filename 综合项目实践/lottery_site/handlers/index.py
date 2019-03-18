7089e2e17199e0bdf1cf5f9b67b1c15af1af701d
fleschier <cyxccy@126.com>
2019-03-18 16:12:25 +0800

    update

--- 综合项目实践/lottery_site/handlers/index.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Python
-# coding=utf-8
-
-import tornado.web
-
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html")
--- 综合项目实践/lottery_site/methods/Util.py
+++
@@ -1,5 +0,0 @@
-import numpy as np 
-
-def getRandIdx(size, num, replace = False):
-    lst = list(range(size))
-    return np.random.choice(lst,num, replace)
